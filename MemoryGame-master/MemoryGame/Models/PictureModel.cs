d77a69f2b7f0f2e4a4c8ce8344f0ec7b019f45d9
Odinosik <35398911+Odinosik@users.noreply.github.com>
2021-09-09 18:06:49 +0000

    Add files via upload

---
+++ MemoryGame-master/MemoryGame/Models/PictureModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MemoryGame.Models
+{
+    public class PictureModel
+    {
+        public int Id { get; set; }
+        public string ImageSource { get; set; }
+
+        //Slide status
+        private bool _isViewed { get; set; }
+        private bool _isMatched { get; set; }
+        private bool _isFailed { get; set; }
+
+    }
+}
